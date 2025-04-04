732b32a3860b614467d98055dccf1452de2b0081
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-04 15:04:24 +0700

    fix errors

---
+++ Bybit Coinsweeper/setup.bat
@@ -0,0 +1,35 @@
+@echo off
+title Bybit Coinsweeper Bot by @MeoMunDep
+color 0A
+
+cd %~dp0
+
+echo Checking configuration files...
+
+
+(for %%F in (datas.txt bybit_ids.txt proxies.txt) do (
+    if not exist %%F (
+        type nul > %%F
+        echo Created %%F
+    )
+))
+
+echo Configuration files checked.
+
+echo Checking dependencies...
+if exist "..\node_modules" (
+    echo Using node_modules from parent directory...
+    cd ..
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent crpypto-js
+    cd %~dp0
+) else (
+    echo Installing dependencies in current directory...
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent crypto-js
+)
+echo Dependencies installation completed!
+
+echo Starting the bot...
+node meomundep
+
+pause
+exit
